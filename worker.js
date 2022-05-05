7fd0902a62ece8446f62f95742f6b95bf5830666
Quentin Helvie <73959954+helvieq499@users.noreply.github.com>
2022-05-05 10:18:52 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,25 +1,24 @@
-controller = undefined;
-connections = {};
+this.connections = {};
+this.controller = null;
 
-onconnect = e => {
-console.log(e);
-  let selector = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER).toString(16)
-  connections[selector] = e.ports[0];
-  e.ports[0].postMessage(["known-as", selector]);
-  e.ports[0].onmessage = e => handle(selector, e.ports[0], e);
-};
+onconnect = event => {
+    let ctx = {
+        port: event.ports[0],
+        identity: Math.floor(Math.random() * Number.MAX_SAFE_INTEGER).toString(16),
+    };
+    this.connections[ctx.identity] = ctx;
+    ctx.port.onmessage = event => handle(ctx, event);
+}
 
-function handle(selector, sender, event) {
-  if (event.data[0] == "set-controller") {
-    controller = sender;
-    delete connections[selector];
-    let keys = Object.keys(connections);
-    for (let key of keys)
-      connections[key].postMessage(["new-controller", selector]);
-  } else if (event.data[0] == "send-message") {
-    connections[e.data[1]].postMessage(["message", selector, event.data[2]]);
-  } else if (event.data[0] == "send-js") {
-    if (sender != controller) return;
-    connections[event.data[1]].postMessage(["js", selector, event.data[2]]);
-  }
+function handle(ctx, event) {
+    console.log(event.data + " from " + ctx.identity);
+    if (event.data[0] == "get-identity")
+        ctx.port.postMessage(["identity", ctx.identity]);
+    else if (event.data[0] == "set-controller") {
+        this.controller = ctx;
+        for (let key of Object.keys(this.connections))
+            this.connections[key].port.postMessage(["controller", ctx.identity]);
+    } else if (event.data[0] == "send-message") {
+        this.connections[event.data[1]]?.port.postMessage(["message", ctx.identity, ctx === controller, event.data[2]]);
+    }
 }
