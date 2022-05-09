e7a41578324a5b0d3d670e6636c544f59a8d60d9
Quentin Helvie <73959954+helvieq499@users.noreply.github.com>
2022-05-09 09:06:41 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,23 +2,28 @@ this.connections = {};
 this.controller = null;
 
 onconnect = event => {
-    let ctx = {
-        port: event.ports[0],
-        identity: Math.floor(Math.random() * Number.MAX_SAFE_INTEGER).toString(16),
-    };
-    this.connections[ctx.identity] = ctx;
-    ctx.port.onmessage = event => handle(ctx, event);
+  let ctx = {
+    port: event.ports[0],
+    identity: Math.floor(Math.random() * Number.MAX_SAFE_INTEGER).toString(16),
+  };
+  this.connections[ctx.identity] = ctx;
+  ctx.port.onmessage = event => handle(ctx, event);
+  broadcast(["connection", ctx.identity]);
 }
 
 function handle(ctx, event) {
-    console.log(event.data + " from " + ctx.identity);
-    if (event.data[0] == "get-identity")
-        ctx.port.postMessage(["identity", ctx.identity]);
-    else if (event.data[0] == "set-controller") {
-        this.controller = ctx;
-        for (let key of Object.keys(this.connections))
-            this.connections[key].port.postMessage(["controller", ctx.identity]);
-    } else if (event.data[0] == "send-message") {
-        this.connections[event.data[1]]?.port.postMessage(["message", ctx.identity, ctx === controller, event.data[2]]);
-    }
+  console.log(event.data + " from " + ctx.identity);
+  if (event.data[0] == "get-identity")
+    ctx.port.postMessage(["identity", ctx.identity]);
+  else if (event.data[0] == "set-controller")
+    broadcast(["controller", (this.controller = ctx).identity]);
+  else if (event.data[0] == "send-message")
+    this.connections[event.data[1]]?.port.postMessage(["message", ctx.identity, ctx === controller, event.data[2]]);
+  else if (event.data[0] == "get-connections")
+    ctx.port.postMessage(["connections", Object.keys(connections)]);
+}
+
+function broadcast(data) {
+  for (let key of Object.keys(this.connections))
+    this.connections[key].port.postMessage(data);
 }
