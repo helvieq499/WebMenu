4d07d71f7b57bf2742fb48fe3a45cdbca3d3ec0e
Quentin Helvie <73959954+helvieq499@users.noreply.github.com>
2022-05-16 10:01:17 -0400

    add plugin button

--- menu/plugin-manager.js
+++ menu/plugin-manager.js
@@ -1,42 +1,48 @@
 plugins = [];
 
 ls = JSON.parse(localStorage["WebMenu"] || "{}");
-ls.save = function() {
-    localStorage["WebMenu"] = JSON.stringify(this);
+ls.save = function () {
+  localStorage["WebMenu"] = JSON.stringify(this);
 };
 
 if (Array.isArray(ls.plugins))
-    for (let plugin of ls.plugins)
-        loadPlugin(plugin)
+  for (let plugin of ls.plugins)
+    loadPlugin(plugin)
+
+function addPlugin(url) {
+  plugins.push(url);
+  plugins.save();
+}
 
 function loadPlugin(url) {
-    if (typeof url == "string") {
-        fetch(url).then(resp => resp.text()).then(data => {
-            let fn;
-            try {
-                fn = new Function(data);
-            } catch (err) {
-                console.error("Failed to compile plugin: " + err)
-                return;
-            }
+  if (typeof url == "string")
+    fetch(url).then(resp => resp.text()).then(loadPluginRaw);
+}
 
-            let ret;
-            try {
-                ret = fn();
-            } catch (err) {
-                console.error("Failed to run plugin: " + err);
-                return;
-            }
+function loadPluginRaw(data) {
+  let fn;
+  try {
+    fn = new Function(data);
+  } catch (err) {
+    console.error("Failed to compile plugin: " + err)
+    return;
+  }
 
-            let inst;
-            try {
-                inst = new ret();
-            } catch (err) {
-                console.error("Failed to create instance of plugin");
-                return;
-            }
+  let ret;
+  try {
+    ret = fn();
+  } catch (err) {
+    console.error("Failed to run plugin: " + err);
+    return;
+  }
 
-            plugins.push(inst);
-        });
-    }
+  let inst;
+  try {
+    inst = new ret();
+  } catch (err) {
+    console.error("Failed to instantiate plugin: " + err);
+    return;
+  }
+
+  plugins.push(inst);
 }
